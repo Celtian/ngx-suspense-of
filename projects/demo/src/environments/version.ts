2744fec025f0d5611dce8805367d6bf3c53dd651
Dominik Hladík <dominik.hladik@seznam.cz>
2022-12-11 09:03:17 +0100

    chore(deps): angular 14

--- projects/demo/src/environments/version.ts
+++ projects/demo/src/environments/version.ts
@@ -1,7 +1,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-  TAG: '1.0.2',
+  TAG: '1.0.3',
   COMMIT_HASH: ''
 };
 /* tslint:enable */
