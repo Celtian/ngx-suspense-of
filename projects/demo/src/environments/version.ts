7ca522b780c2ec08efd5d2d2fe686b27394c7251
Dominik Hladík <dominik.hladik@seznam.cz>
2023-03-15 23:56:52 +0100

    chore(package): package changed

--- projects/demo/src/environments/version.ts
+++
@@ -1,7 +0,0 @@
-// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
-/* tslint:disable */
-export const VERSION = {
-  TAG: '1.0.4',
-  COMMIT_HASH: ''
-};
-/* tslint:enable */
