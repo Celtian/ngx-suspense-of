b505efe5881a33714b0569ea75b0085ea703ba2a
Celtian <dominik.hladik@seznam.cz>
2026-06-25 13:50:46 +0200

    chore(deps): updated to angular 22

--- scripts/create-version.ts
+++ scripts/create-version.ts
@@ -14,9 +14,7 @@ const file = resolve(__dirname, '..', 'projects', 'demo', 'src', 'environments',
 writeFileSync(
   file,
   `// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
-/* eslint-disable */
 export const VERSION = ${JSON.stringify(VERSION, null, 4)};
-/* eslint-enable */
 `,
   { encoding: 'utf-8' }
 );
